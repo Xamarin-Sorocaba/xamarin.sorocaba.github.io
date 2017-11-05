9eb89b51c7862d47b55b12fea52341619bdaabfb
Juliano Custódio <juliano.custodio@hotmail.com.br>
2017-11-05 09:28:36 -0200

    Adiciona palestras

--- js/scripts.js
+++ js/scripts.js
@@ -155,11 +155,11 @@ $(window).resize(function () {
 
 // iPhone Header Carousel
 $('header .carousel').carousel({
-  interval: 3000
+  interval: 5000
 })
 
 // iPhone Features Carousel
 $('.detail .carousel').carousel({
-  interval: 4000
+  interval: 6000
 })
 
